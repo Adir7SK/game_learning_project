52eeb8cf2560b09940044855c0821d4657908d06
Adir Saly-Kaufmann <adir7saly@gmail.com>
2022-07-26 17:34:50 +0200

    Updating Good Character

--- src/Characters/Character.py
+++ src/Characters/Character.py
@@ -7,21 +7,26 @@ class Character(metaclass=ABCMeta):
     Here we will define all the characteristics of a character in the game (all types of characters).
     """
 
-    def __init__(self, max_life, life_remain):
-        self._max_life = max_life
-        self._life_remain = life_remain
+    def __init__(self, life):
+        if not isinstance(life, (float, int)) or life <= 0:
+            raise TypeError("Character's life must be a positive number.")
+        self._life = life
 
     @property
     def life_remain(self):
         """Getter method - returns life character currently has left"""
-        return self._life_remain
+        return self._life
 
     @life_remain.setter
     def life_remain(self, damage):
         """Setter method to updates life after attack"""
         if type(damage) != int and type(damage) != float:
             raise TypeError("The attack is from a wrong data type.")
-        self._life_remain -= damage
+        self._life -= damage
+
+    @life_remain.deleter
+    def life_remain(self):
+        raise AttributeError("Life remaining is an attribute that cannot be deleted.")
 
     @abc.abstractmethod
     def items(self):
--- src/Characters/Good_Character.py
+++ src/Characters/Good_Character.py
@@ -2,17 +2,80 @@ from Character import Character
 
 
 class GoodCharacter(Character):
+    """
+    We don't require a shield here, as some character can exist without having one.
+    We need to build a tree for the weapons and shields, which this class can get the weapons from.
+    Create Aids.
+    """
+    def __init__(self, life, undercover,  *items):
+        if type(undercover) != bool:
+            raise TypeError("Character's undercover value must be a boolean value.")
+        weapon_serial, shield_serial, aids_serial = None, None, []
+        for serial_number in items:
+            if serial_number[:6] not in ["Weapon", "Shield"] and serial_number[:3] != "Aid":
+                raise ValueError("The items a character can carry are either a Weapon, Shield, or an Aid.")
+            if serial_number[:6] == "Weapon":
+                if weapon_serial:
+                    raise AttributeError("Character must have one weapon at most.")
+                else:
+                    weapon_serial = serial_number
+            elif serial_number[:6] == "Shield":
+                if shield_serial:
+                    raise AttributeError("Character must have one shield at most.")
+                else:
+                    shield_serial = serial_number
+            else:
+                aids_serial.append(serial_number)
 
-    def __init__(self, max_life, life_remain, weapon, shield, *items):
-        super().__init__(max_life, life_remain)
-        self._weapon = weapon
-        self._shield = shield
+        if not weapon_serial:
+            raise AttributeError("Character must have one weapon and at most one shield.")
+        super().__init__(life)
+        print("Life remaining: ", self._life)
+        print("Weapons available: ", weapon_serial)
+        print("Shields available: ", shield_serial)
+        print("Aids available: ", aids_serial)
+        #self._weapon = weapon
+        #self._shield = shield
+        self._undercover = undercover
         self._items = list(items)
 
-    def items(self):                  # This kind of character must have a weapon, shield, and potentially a few items.
+    def items(self):                  # This kind of character must have a weapon, shield, and potentially more items.
         """List of items that character carries"""
         raise NotImplementedError
 
     def symbol(self):
         """The symbol on the printed maze that identifies the character"""
         raise NotImplementedError
+
+    @property
+    def attack(self):
+        """Here we return the speed and strength of the weapon"""
+        raise NotImplementedError
+
+    @property
+    def defend(self):
+        """Here we return the speed and strength of the shield"""
+        raise NotImplementedError
+
+    @property
+    def weapon_info(self):
+        """This should return relevant information about the weapon."""
+        raise NotImplementedError
+
+    @property
+    def shield_info(self):
+        """This should return relevant information about the shield."""
+        raise NotImplementedError
+
+    @property
+    def aids_info(self):
+        """This should return relevant information about all the aids this character has."""
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    character = GoodCharacter(1200, True, "Weapon123", "Shield123")
+    character.life_remain = 12
+    print(character.life_remain)
+    # del character.life_remain
+
