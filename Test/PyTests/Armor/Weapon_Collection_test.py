3e8e2b5faf282b15cb2aeab0768b0c737295f4d8
Adir Saly-Kaufmann <adir7saly@gmail.com>
2022-09-18 01:14:55 +0200

    Adding another test case

--- Test/PyTests/Armor/Weapon_Collection_test.py
+++ Test/PyTests/Armor/Weapon_Collection_test.py
@@ -14,6 +14,7 @@ def full_tree():
     weapon_collection.insert_weapon("Medium Wooden Sword", 1, cs.wood_african_mahogany, 20, cs.slim + " " + cs.even, False, "Woosh")
     weapon_collection.insert_weapon("Good Metal Gun", 1, cs.iron, 70, cs.slim + " " + cs.even, True, "Baam")
     weapon_collection.insert_weapon("Medium Wooden Sword Heavy", 15, cs.inconel, 10, cs.broad + " " + cs.even, False, "Woooh")
+    weapon_collection.insert_weapon("MediumMetal Sword Light", 0.15, cs.iron, 50, cs.slim + " " + cs.even, False, "Shiing")
     weapon_collection.insert_weapon("Excellent Metal Gun", 0.5, cs.titanium, 99.9, cs.slim + " " + cs.short, True, "HHH")
     weapon_collection.print_helper()
     return weapon_collection
@@ -21,11 +22,13 @@ def full_tree():
 
 @pytest.mark.parametrize("obj, expected_name",
                          [(Weapon("Medium Wooden Sword", 1, cs.wood_african_mahogany, 20, cs.slim + " " + cs.even, False, "Woosh"), "Medium Wooden Sword"),
-                          (Weapon("Medium Wooden Sword", 5, cs.inconel, 10, cs.broad + " " + cs.even, False, "Woooh"), "Medium Wooden Sword"),
+                          (Weapon("Medium Wooden Sword", 1, cs.iron, 70, cs.slim + " " + cs.even, True, "Woooh"), "Good Metal Gun"),
                           (Weapon("Excellent Metal Gun", 0.5, cs.titanium, 99.9, cs.slim + " " + cs.short, True, "HHH"), "Excellent Metal Gun"),
+                          (Weapon("Excellent Metal Gun", 15, cs.inconel, 10, cs.broad + " " + cs.even, False, "HHH"), "Medium Wooden Sword Heavy"),
+                          (Weapon("Excellent Metal Gun", 3, cs.wood_african_mahogany, 50, cs.broad + " " + cs.even, True, "HHH"), "Low Wooden Gun"),
+                          (Weapon("Excellent Metal Gun", 0.15, cs.iron, 50, cs.slim + " " + cs.even, False, "HHH"), "MediumMetal Sword Light"),
                           ])
 def test_search_on_tree(full_tree, obj, expected_name):
     """Searching a valid existing serial number works."""
     serial_n = obj.serial_number_int()
-    print(full_tree.search(serial_n).name())
     assert full_tree.search(serial_n).name() == expected_name
