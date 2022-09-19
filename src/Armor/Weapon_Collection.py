a21bead775eb6ba2e7957e8e02ecdb5c51f436f5
Adir Saly-Kaufmann <adir7saly@gmail.com>
2022-09-19 22:11:14 +0200

    Removing unnecessary testing

--- src/Armor/Weapon_Collection.py
+++ src/Armor/Weapon_Collection.py
@@ -1,6 +1,5 @@
 from src.Armor.Weapon import Weapon
 from src.Common_general_functionalities.Binary_Tree import AVL
-from src.Common_general_functionalities import common_strings as cs
 
 
 class WeaponCollection:
@@ -19,12 +18,3 @@ class WeaponCollection:
     def print_helper(self):
         """Call this method as follows: TREE_OBJECT_NAME.print_helper()"""
         AVL().print_helper(self.weapon_collection)
-
-
-if __name__ == "__main__":
-    w = WeaponCollection("Some Weapon", 3, cs.wood_african_mahogany, 50, cs.broad + " " + cs.even, True, "Boom")
-    w.insert_weapon("Another Weapon", 1, cs.wood_african_mahogany, 20, cs.slim + " " + cs.even, False, "Woosh")
-    w.insert_weapon("Strong Weapon", 1, cs.iron, 70, cs.slim + " " + cs.even, True, "Baam")
-    w.print_helper()
-    ww = Weapon("Something", 1, cs.wood_african_mahogany, 20, cs.slim + " " + cs.even, False, "Woosh") # Showing that creating another object with the same values as another one will have the same serial number
-    print(w.search(ww.serial_number_int()).name())
